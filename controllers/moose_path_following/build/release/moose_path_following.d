6de879085c32e7f6fc7892c810b281892e824384
mcswing <maochuan.swing@gmail.com>
2024-06-22 23:16:20 +0200

    feat   add muscle

---
+++ controllers/moose_path_following/build/release/moose_path_following.d
@@ -0,0 +1,8 @@
+build/release/moose_path_following.o: moose_path_following.c \
+ D:\webot\Webots/include/controller/c/webots/compass.h \
+ D:\webot\Webots/include/controller/c/webots/types.h \
+ D:\webot\Webots/include/controller/c/webots/gps.h \
+ D:\webot\Webots/include/controller/c/webots/keyboard.h \
+ D:\webot\Webots/include/controller/c/webots/motor.h \
+ D:\webot\Webots/include/controller/c/webots/robot.h \
+ D:\webot\Webots/include/controller/c/webots/nodes.h
